034b08a7a25f9ac15ca94d8f404ffadfde2e605a
Nick <hello@swiftful-thinking.com>
2023-12-10 16:53:17 -0500

    AuthManagerEnvironmentKey

---
+++ Sources/SwiftfulFirebaseAuth/Core/AuthManagerEnvironmentKey.swift
@@ -0,0 +1,20 @@
+//
+//  AuthManagerEnvironmentKey.swift
+//
+//
+//  Created by Nick Sarno on 12/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+public struct AuthManagerEnvironmentKey: EnvironmentKey {
+    public static let defaultValue: AuthManager = AuthManager(configuration: .mock)
+}
+
+public extension EnvironmentValues {
+    var auth: AuthManager {
+        get { self[AuthManagerEnvironmentKey.self] }
+        set { self[AuthManagerEnvironmentKey.self] = newValue }
+    }
+}
