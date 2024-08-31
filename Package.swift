7a2070c0fa165e5d92b316f8ee5a4ca26d204531
Nick <hello@swiftful-thinking.com>
2024-08-31 16:35:21 -0400

    v10_15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftfulFirebaseAuth",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
